a138150596090e30643ad329e82914fb1f810e6f
R3alS1rus <135262108+R3alS1rus@users.noreply.github.com>
2023-06-09 14:06:43 +0200

    Homework 09/06/23
    1 уровень сложности: напишите код, который при запуске выведет в консоль:
    
    /'"работу выполнил/а ваше_имя"'\
    
    (вывести спец символы в консоль - обязательное условие, посмотрите дополнительно тему "экранирование символов")
    
    
    создайте 8 переменных для всех 8 примитивных типов данных, распечатайте значение каждой,
    затем распечатайте пустую строку, чтобы отделить следующий вывод в консоль от предыдущего,
    измените значения ваших переменных, не создавая новых и распечатайте их вновь.
    
    
    доп материалы:
    https://www.youtube.com/watch?v=AnRKrnbiVzU
    https://www.youtube.com/watch?v=U0U8Ddx4TgE

---
+++ src/basicJava.java
@@ -0,0 +1,46 @@
+public class basicJava {
+    public static void main(String[] args){
+
+        System.out.println("\"Работу выполнил Даниэль Шультайс\"");
+        System.out.println();
+
+boolean danielBoolean = true;
+byte danielByte = 1;
+short danielShort = 15_000;
+int danielInt = 10_000_000;
+long danielLong = 250L;
+float danielFloat = 31.458F;
+double danielDouble = 2.5555;
+char danielChar = 'W';
+
+        System.out.println(danielBoolean);
+        System.out.println(danielByte);
+        System.out.println(danielShort);
+        System.out.println(danielInt);
+        System.out.println(danielLong);
+        System.out.println(danielFloat);
+        System.out.println(danielDouble);
+        System.out.println(danielChar);
+
+        System.out.println();
+
+danielBoolean = false;
+danielByte = 2;
+danielShort = 16_000;
+danielInt = 20_000_000;
+danielLong = 500L;
+danielFloat = 60.897F;
+danielDouble = 7.333;
+danielChar = 'L';
+
+        System.out.println(danielBoolean);
+        System.out.println(danielByte);
+        System.out.println(danielShort);
+        System.out.println(danielInt);
+        System.out.println(danielLong);
+        System.out.println(danielFloat);
+        System.out.println(danielDouble);
+        System.out.println(danielChar);
+
+}
+}
